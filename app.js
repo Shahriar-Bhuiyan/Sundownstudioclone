c16dca088b11f5cdd1b6d8aedbcbb3cd8c88faec
shahriar-bhuiyan <Shahriarbhuiyan1@gmail.com>
2024-01-09 04:07:34 +0600

    mousehovereffect

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+// var a = document.querySelector('#elem1');
+// var image = a.getAttribute('data-img')
+
+var elemC = document.querySelector("#elem-container");
+var fixed = document.querySelector('#fixed-image')
+elemC.addEventListener('mouseenter',function(){
+     fixed.style.display = "block"
+})
+elemC.addEventListener('mouseleave',function(){
+    fixed.style.display = "none"
+});
+
+var elme = document.querySelectorAll('.elem');
+  elme.forEach((value,i)=>{
+    value.addEventListener('mouseenter',()=>{
+        var image = value.getAttribute("data-img");
+         fixed.style.backgroundImage = `url(${image})`
+      })
+  })
