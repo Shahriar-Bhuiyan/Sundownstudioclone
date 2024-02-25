b0fbe155500d322a60e2b68166883a6abef7d433
shahriar-bhuiyan <Shahriarbhuiyan1@gmail.com>
2024-02-25 22:09:32 +0600

    slider fix

--- app.js
+++ app.js
@@ -21,58 +21,80 @@ elme.forEach((value, i) => {
 });
 
 var slider = document.querySelector(".swiper");
-var pointer = document.querySelector(".pointer");
+// var pointer = document.querySelector(".pointer");
 
-slider.addEventListener("mousemove", function (e) {
-  gsap.to(pointer, {
-    duration: 0.3,
-    opacity: 1,
-    left: e.clientX,
-    top: e.clientY,
-    ease: "power2.out",
-    display: "flex",
-  });
-});
+// slider.addEventListener("mousemove", function (e) {
+//   gsap.to(pointer, {
+//     duration: 0.3,
+//     opacity: 1,
+//     left: e.clientX,
+//     top: e.clientY,
+//     ease: "power2.out",
+//     display: "flex",
+//   });
+// });
 
-slider.addEventListener("mouseleave", function (e) {
-  gsap.fromTo(
-    pointer,
-    { opacity: 1 },
-    {
-      opacity: 0,
-      duration: 0.3,
-      scale: -1,
-      onComplete: () => (pointer.style.display = "none"),
-    }
-  );
-});
+// slider.addEventListener("mouseleave", function (e) {
+//   gsap.fromTo(
+//     pointer,
+//     { opacity: 1 },
+//     {
+//       opacity: 0,
+//       duration: 0.3,
+//       scale: -1,
+//       onComplete: () => (pointer.style.display = "none"),
+//     }
+//   );
+// });
 
 var menu = document.querySelector(".mobile_menu");
 var full = document.querySelector("#full-src");
 var navImg = document.querySelector('nav img')
 var click = false;
 
+var icon = document.querySelector('.fa-solid')
+
 menu.addEventListener("click", () => {
   if (!click) {
     click = true;
     full.style.top = 0;
     navImg.style.opacity = 0
+    
+    icon.classList.add('fa-xmark');
+    icon.classList.remove('fa-bars');
+    
 
   } else {
     click = false;
     full.style.top = -100 + "%";
     navImg.style.opacity = 1
+    icon.classList.add('fa-bars');
+    icon.classList.remove('fa-xmark');
+    
   }
 
-  console.log(click);
 });
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
+  slidesPerView: 1,
+  spaceBetween: 20,
   centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
   },
 });
